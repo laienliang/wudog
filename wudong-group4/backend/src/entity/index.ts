c7658c5b77de8d68cef6832d62234a9fec7d79fd
jk2747700735-maker <jk2747700735@gmail.com>
2026-06-25 17:34:28 +0800

    删除master分支内误上传的wudong-group4文件夹

--- wudong-group4/backend/src/entity/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./ScenicSpot";
-export * from "./TicketType";
-export * from "./RoutePackage";
-export * from "./RouteItinerary";
-export * from "./TicketOrder";
-export * from "./AdminUser";
