12051c325b5527a786197586f582371a17d3bfb6
laien <lianglaiyang@163.com>
2026-06-30 08:24:10 +0800

    修复餐厅主图更新缓存问题

--- cool-admin-midway/src/modules/food/service/restaurant.ts
+++ cool-admin-midway/src/modules/food/service/restaurant.ts
@@ -1,5 +1,5 @@
 import { Provide } from '@midwayjs/core';
-import { CoolCache, BaseService } from '@cool-midway/core';
+import { BaseService } from '@cool-midway/core';
 import { InjectEntityModel } from '@midwayjs/typeorm';
 import { Repository } from 'typeorm';
 import { FoodRestaurantEntity } from '../entity/restaurant';
@@ -8,19 +8,4 @@ import { FoodRestaurantEntity } from '../entity/restaurant';
 export class FoodRestaurantService extends BaseService {
   @InjectEntityModel(FoodRestaurantEntity)
   foodRestaurantEntity: Repository<FoodRestaurantEntity>;
-
-  @CoolCache(30 * 60) // 30 分钟缓存
-  async list(query?, option?, connectionName?) {
-    return super.list(query, option, connectionName);
-  }
-
-  @CoolCache(30 * 60) // 30 分钟缓存
-  async page(query?, option?, connectionName?) {
-    return super.page(query, option, connectionName);
-  }
-
-  @CoolCache(30 * 60) // 30 分钟缓存
-  async info(id, infoIgnoreProperty?) {
-    return super.info(id, infoIgnoreProperty);
-  }
 }
